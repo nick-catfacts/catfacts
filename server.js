85bbd3662f6fac7f47dac85f05612c9dc643b6ae
Your Name <you@example.com>
2016-10-04 19:29:35 -0700

    upd

--- server.js
+++ server.js
@@ -144,5 +144,11 @@ app.on('stormpath.ready',function(){
 });
 
 
-app.listen(3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+http.createServer(app).listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
